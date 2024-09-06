d122dedc4317ec25813395e42171218fab7a43d6
EduardoHaibleForti <eduardo.hf@puccampinas.edu.br>
2024-09-06 11:44:13 -0300

    Add files via upload

---
+++ listateorica2a.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int num1, num2, num3;
+
+    printf("Digite o primeiro numero :");
+    scanf("%d",&num1);
+
+    printf("Digite o segundo numero :");
+    scanf("%d",&num2);
+
+    printf("Digite o terceiro numero :");
+    scanf("%d",&num3);
+
+    printf("Os numeros na ordem inversa sao : %d,%d,%d",num3, num2, num1);
+
+    return 0;
+
+
+
+}
